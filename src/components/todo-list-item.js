942e033a8145473bc12baaee82143062b55d2324
d.kozlov <d.kozlov@hottelecom.by>
2020-11-18 11:44:48 +0300

    add props to todo list item

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const TodoListItem = () => {
-    return <span>Drink Coffee</span>;
+const TodoListItem = ({ label, important = false }) => {
+
+    const style = {
+        color: important ? 'tomato' : ''
+    };
+
+    return <span style={style}>{ label }</span>;
 };
 
 export default TodoListItem;
