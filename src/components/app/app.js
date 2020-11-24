623041d180067f932a8d75722d442dde5e4d0311
d.kozlov <d.kozlov@hottelecom.by>
2020-11-24 16:03:03 +0300

    restore toggle property

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,11 +14,20 @@ export default class App extends Component {
 
 	state = {
 		todoData: [
-			{ label: 'Drink Coffee', important: false, id: 1 },
-			{ label: 'Make Awesome App', important: true, id: 2 },
-			{ label: 'Have a lunch', important: false, id: 3 }
+			this.createTodoItem('Drink Coffee'),
+			this.createTodoItem('Make Awesome App'),
+			this.createTodoItem('Have a lunch')
 		]
 	};
+
+	createTodoItem(label) {
+		return {
+			label,
+			done: false,
+			important: false,
+			id: this.maxId++
+		};
+	}
 
 	deleteItem = (id) => {
 		
@@ -35,12 +44,8 @@ export default class App extends Component {
 		});
 	};
 
-	addItem = (label) => {
-		const newItem = {
-			label,
-			important: false,
-			id: this.maxId++
-		};
+	addItem = (text) => {
+		const newItem = this.createTodoItem(text);
 
 		this.setState(({ todoData }) => {
 			const newArray = [
@@ -54,20 +59,42 @@ export default class App extends Component {
 		});
 	};
 
+	toggleProperty(arr, id, propName) {
+		const idx = arr.findIndex(el => el.id === id);
+		const oldItem = arr[idx];
+		const newItem = {...oldItem, [propName]: !oldItem.[propName]};
+
+		return [
+			...arr.slice(0, idx),
+			newItem,
+			...arr.slice(idx + 1)
+		];
+	}
+
 	onToggleImportant = (id) => {
-		console.log('Toggle Important', id);
+		this.setState(({ todoData }) => {
+			return {
+				todoData: this.toggleProperty(todoData, id, 'important')
+			};
+		});
 	};
 
 	onToggleDone = (id) => {
-		console.log('Toggle Done', id);
+		this.setState(({ todoData }) => {
+			return {
+				todoData: this.toggleProperty(todoData, id, 'done')
+			};
+		});
 	};
 
 	render() {
 		const { todoData } = this.state;
+		const doneCount = todoData.filter((el) => el.done).length;
+		const todoCount = todoData.length - doneCount;
 
 		return (
 			<div className="todo-app">
-				<AppHeader toDo={1} done={3} />
+				<AppHeader toDo={todoCount} done={doneCount} />
 				<div className="top-panel d-flex">
 					<SearchPanel />
 					<ItemStatusFilter />
